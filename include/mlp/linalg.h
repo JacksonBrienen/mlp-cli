77fd7e40cf5488c541ff7fbf252f4e1000b9eb53
Jackson Brienen <brienenj@cwu.edu>
2024-12-03 07:21:24 -0800

    Added linalg definitions

--- include/mlp/linalg.h
+++ include/mlp/linalg.h
@@ -1,7 +1,8 @@
 #ifndef __MLP_LINALG_H__
 #define __MLP_LINALG_H__
 
-#include <stddef.h>
+#include <stdlib.h>
+#include "smalloc.h"
 
 /*
  * Matrix struct holds a matrix, usually in heap memory and created by built in functions.
@@ -25,6 +26,37 @@ typedef struct {
     float *vector;
 } vector_t;
 
-// TODO
+matrix_t *matrix(size_t rows, size_t cols, float m[rows][cols]);
+
+vector_t *vector(size_t length, float v[length]);
+
+void $matrix(matrix_t *m);
+
+void $vector(vector_t *v);
+
+#define is_vector(a) (__is_vector((matrix_t *)(a)))
+bool __is_vector(matrix_t *m);
+
+#define mat_add(a, b) __mat_add((matrix_t *)a, (matrix_t*)b)
+matrix_t *__mat_add(matrix_t *a, matrix_t *b);
+
+#define mat_sub(a, b) __mat_sub((matrix_t *)a, (matrix_t*)b)
+matrix_t *__mat_sub(matrix_t *a, matrix_t *b);
+
+#define mat_mul(a, b) (__mat_mul((matrix_t *)(a), (matrix_t *)(b)))
+matrix_t *__mat_mul(matrix_t *a, matrix_t *b);
+
+vector_t *dot(vector_t *a, vector_t *b);
+
+double sum(vector_t *a);
+
+#define mcopy(m) (typeof((m)))__mcopy((matrix_t *)(m))
+matrix_t *__mcopy(matrix_t *m);
+
+#define scale(scalar, mat) (typeof(mat))__scale(scalar, (matrix_t *)(mat))
+matrix_t *__scale(double scalar, matrix_t *mat);
+
+#define transpose(m) __transpose((matrix_t*)(m))
+matrix_t *__transpose(matrix_t *m);
 
 #endif
